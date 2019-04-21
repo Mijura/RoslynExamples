e5de1c027989389826677e7a5dfc952b6b78cf89
Mijura <miodragvilotijevic@gmail.com>
2019-04-21 16:14:44 +0200

    added example to display syntax tree for some class

---
+++ RoslynExamples/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.CodeAnalysis.CSharp;
+using System.IO;
+
+namespace RoslynExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tree = CSharpSyntaxTree.ParseText(File.ReadAllText("../../../Program.cs"));
+            TreeWalker treeWalker = new TreeWalker();
+            //visit and display syntax tree
+            treeWalker.Visit(tree.GetRoot());
+        }
+    }
+}
---
+++ RoslynExamples/TreeWalker.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
+
+namespace RoslynExamples
+{
+    public class TreeWalker: CSharpSyntaxWalker
+    {
+        public TreeWalker() : base(SyntaxWalkerDepth.StructuredTrivia)
+        {
+        }
+
+        public override void Visit(SyntaxNode node)
+        {
+            Console.WriteLine(node.Kind());
+            base.Visit(node);
+        }
+
+        public override void VisitToken(SyntaxToken token)
+        {
+            Console.WriteLine(String.Format("{0} {1}", token.Kind(), token));
+            base.VisitToken(token);
+        }
+    }
+}
