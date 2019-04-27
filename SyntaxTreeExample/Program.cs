2f5f3d0101a7dcc15bcf4ccf78f5623acb212d3e
Mijura <miodragvilotijevic@gmail.com>
2019-04-27 21:18:38 +0200

    Added example to check for errors in sintax tree

--- SyntaxTreeExample/Program.cs
+++ SyntaxTreeExample/Program.cs
@@ -1,8 +1,10 @@
 ﻿using CodeGenerator;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using RoslynExamples;
 using System;
 using System.IO;
+using System.Linq;
 
 namespace SyntaxTreeExample
 {
@@ -11,14 +13,24 @@ namespace SyntaxTreeExample
      */
     class Program
     {
+
         static void Main(string[] args)
         {
             var tree = CSharpSyntaxTree.ParseText(File.ReadAllText("../../../Program.cs"));
             TreeWalker treeWalker = new TreeWalker();
             //visit and display syntax tree
             treeWalker.Visit(tree.GetRoot());
+
+            // use class created in design time
             var hello = new MladenWorld();
             hello.PrintHello();
+
+            tree = CSharpSyntaxTree.ParseText(@"class{}");
+
+            //get first error (also we could get warnings)
+            var error = tree.GetDiagnostics().Where(n => n.Severity == DiagnosticSeverity.Error).First();
+            Console.WriteLine(error.ToString());
+
         }
     }
     [DuplicateWithSuffix("World")]
