b03a3d0e2fee0bbbc01352f2d6477ef219d8db7c
Mijura <miodragvilotijevic@gmail.com>
2019-04-27 10:50:12 +0200

    Fixed get compilation by installing MSBuildLocator

--- SymbolApiExample/Program.cs
+++ SymbolApiExample/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.CodeAnalysis.MSBuild;
+﻿using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.MSBuild;
 using System;
 using System.Linq;
 
@@ -9,15 +11,29 @@ namespace SymbolApiExample
         static void Main(string[] args)
         {
             //workspace api
+            MSBuildLocator.RegisterDefaults();
+
             var work = MSBuildWorkspace.Create();
             var solution = work.OpenSolutionAsync(@"..\..\..\..\RoslynExamples.sln").Result;
-            var project = solution.Projects.FirstOrDefault(p => p.Name == "SymbolApiExample");
+            var project = solution.Projects.FirstOrDefault(p => p.Name == "SyntaxTreeExample");
             if (project == null)
-                throw new Exception("Could not find the SymbolApiExample project.");
+                throw new Exception("Could not find the SyntaxTreeExample project.");
             var compilation = project.GetCompilationAsync().Result;
+            ReviewSymbolTable(compilation);
+        }
 
-            var b = compilation.GlobalNamespace;
-
+        private static void ReviewSymbolTable(Compilation compilation)
+        {
+            foreach (var member in compilation.Assembly.GlobalNamespace.GetMembers()
+                .Where(member => member.CanBeReferencedByName))
+            {
+                Console.WriteLine(member.Name);
+                foreach (var item in member.GetTypeMembers()
+                    .Where(item => item.CanBeReferencedByName))
+                {
+                    Console.WriteLine("\t{0}:{1}", item.TypeKind, item.Name);
+                }
+            }
         }
     }
 }
